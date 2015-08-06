1f10d1648fc624a70ca747e1c736c3fa170bd7fd
macgyverfan <geophysman@gmail.com>
2015-08-06 19:42:04 +0100

    First commit

---
+++ HelloWorld/src/application.java
@@ -0,0 +1,14 @@
+
+public class application {
+
+	public static void main(String[] args) {
+		//System.out.println("Hello World!");
+		
+		int myNumber=88;
+		
+		char myChar = 'y';
+		boolean myBoolean = false;
+		System.out.println(myNumber);
+	}
+
+}
